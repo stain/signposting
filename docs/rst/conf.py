7485eb5f128b7cb7a263b998e4bd9af9dc47f758
stain <stain@users.noreply.github.com>
2022-05-17 12:56:48 +0000

    [SKIP] version bump 0.1.1 -> 0.1.2
    Use rdflib for URIRef
    
    ... perhaps a bit excessive import

--- docs/rst/conf.py
+++ docs/rst/conf.py
@@ -48,7 +48,7 @@ project = 'Python-Project-Skeleton'
 year = '2022'
 author = 'Stian Soiland-Reyes'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.1.1'
+version = release = '0.1.2'
 
 pygments_style = 'trac'
 templates_path = ['.']
