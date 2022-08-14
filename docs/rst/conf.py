190fd819758941c560583d0ff329f1bc6618d32c
stain <stain@users.noreply.github.com>
2022-08-14 10:48:35 +0000

    [SKIP] version bump 0.5.0 -> 0.5.1
    Typo in RST broke build

--- docs/rst/conf.py
+++ docs/rst/conf.py
@@ -47,7 +47,7 @@ project = 'signposting link parser'
 year = '2022'
 author = 'Stian Soiland-Reyes'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.5.0'
+version = release = '0.5.1'
 
 pygments_style = 'trac'
 templates_path = ['.']
