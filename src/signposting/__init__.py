dc1eff3ca4ee3d61e0b2605d896a72449909863a
stain <stain@users.noreply.github.com>
2024-02-25 10:25:38 +0000

    [SKIP] version bump 0.9.6 -> 0.9.7
    Merge branch 'main' into cli-merge

--- src/signposting/__init__.py
+++ src/signposting/__init__.py
@@ -47,7 +47,7 @@ discover/merge these concurrently.
 .. _Link set: <https://signposting.org/FAIR/#linksetrec>
 """
 
-__version__ = '0.9.6'
+__version__ = '0.9.7'
 
 from typing import List
 import warnings
